d9162139226f2d79b74893bd0f3fbd4ff7710412
Avinash kumar Mahato <akr34206@gmail.com>
2022-07-28 10:09:53 +0530

    Update index.js

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,7 +2,7 @@ import { put, takeLatest, all } from 'redux-saga/effects';
 
 function* fetchNews() {
 
-  const json = yield fetch('https://newsapi.org/v1/articles?source=cnn&apiKey=c39a26d9c12f48dba2a5c00e35684ecc')
+  const json = yield fetch('https://newsapi.org/v1/articles?source=cnn&apiKey=4085c7ed33a743a19e75849f0db379c9')
     .then(response => response.json());
 
   yield put({ type: "NEWS_RECEIVED", json: json.articles || [{ error: json.message }] });
